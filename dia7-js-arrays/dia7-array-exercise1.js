7f917b617a3837a67dfa90e6a948ac7d4d5349b1
jhon-npc <jh.nicolaspacheco@gmail.com>
2026-09-01 23:27:02 -0500

    resolucion del ejercicio

--- dia7-js-arrays/dia7-array-exercise1.js
+++ dia7-js-arrays/dia7-array-exercise1.js
@@ -1,18 +1,35 @@
 /* Vas a construir un gestor de lista de tareas pendientes (To-Do List) */
 const tareas = ["Comprar víveres", "Estudiar JavaScript", "Llamar al dentista"]; 
 
-/* Muestra la cantidad de tareas que hay actualmente en la lista, usando .length */
+/* Muestra la cantidad de tareas que hay actualmente en la lista*/
+console.log(`${tareas.length} tareas`);
 
-/* Usa .push() para agregar una nueva tarea: "Pagar servicios" */
+/* Agrega una nueva tarea: "Pagar servicios" */
+tareas.push("Pagar servicios");
 
-/* Usa .unshift() para agregar una tarea urgente al inicio de la lista: "Enviar informe" */
+/* Agrega una tarea urgente al inicio de la lista: "Enviar informe" */
+tareas.unshift("Enviar informe");
 
-/* Muestra la lista completa de tareas ya actualizada, recorriéndola con for...of y numerando cada tarea así: 1. Enviar informe */
+/* Muestra la lista completa de tareas ya actualizada numerando cada tarea así: 1. Enviar informe */
+let contador = 0;
+for(const tarea of tareas){
+    contador+=1
+    console.log(`${contador}. ${tarea}`);
+}
 
-/* Usa .includes() para verificar si la tarea "Estudiar JavaScript" sigue en la lista, y muestra un mensaje según el resultado */
+/* Verifica si la tarea "Estudiar JavaScript" sigue en la lista, y muestra un mensaje según el resultado */
+const estudiarJavaScript = "Estudiar JavaScript"
+const evaluarInclude = tareas.includes(estudiarJavaScript);
+console.log(`${estudiarJavaScript}: ${evaluarInclude?"Pendiente por realizar":"Tarea finalizada"}`);
 
-/* Simula que completaste la primera tarea de la lista: usa .shift() para quitarla, y guarda en una variable la tarea que fue eliminada. Muestra un mensaje: "Tarea completada: Enviar informe" */
+/* Simula que completaste la primera tarea de la lista: quitala, y guarda en una variable la tarea que fue eliminada. Muestra un mensaje: "Tarea completada: Enviar informe" */
+const tareaTerminada = tareas.shift();
+console.log(`Tarea completada: ${tareaTerminada}`);
 
-/* Usa .join(" | ") para mostrar todas las tareas restantes en una sola línea, separadas por | */
+/* Muestra todas las tareas restantes en una sola línea, separadas por " | " */
+const formatoString = tareas.join(" | ");
+console.log(formatoString);
 
-/* Usa .indexOf() para encontrar en qué posición está la tarea "Pagar servicios" dentro de la lista actual */
+/* Encuentra en qué posición está la tarea "Pagar servicios" dentro de la lista actual */
+const posicionTarea = tareas.indexOf("Pagar servicios");
+console.log(`Se encuentra en la posicion: ${posicionTarea}`)
